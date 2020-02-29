7b25fd619a999bd35c5e5eff1fb866eb58f1214c
Devin Piner <devpindroid@gmail.com>
2020-02-28 22:11:38 -0700

    testing /movies PUT jwt

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ router.get('/movies', function(req, res) {
         query: Object.keys(req.query).length === 0 ? null : req.query,
         env: process.env.SECRET_KEY
     });
-}).put('/movies', passport.authenticate('jwt', {session: false}, function(req, res) {
+}).put('/movies', passport.authenticate('jwt', {session: false}), function(req, res) {
     res.json({
         status: 200,
         message: "movie updated",
@@ -117,7 +117,7 @@ router.get('/movies', function(req, res) {
         query: Object.keys(req.query).length === 0 ? null : req.query,
         env: process.env.SECRET_KEY
     });
-}));
+});
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
